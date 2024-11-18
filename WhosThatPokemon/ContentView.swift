83b34c38aac914d8ab58f5ff79360cbecd3c2b76
Rob Wilson <rob.wilson195@gmail.com>
2024-11-18 12:34:47 +0000

    Initial Commit

---
+++ WhosThatPokemon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhosThatPokemon
+//
+//  Created by Catherine Megregian on 18/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhosThatPokemon/WhosThatPokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhosThatPokemonApp.swift
+//  WhosThatPokemon
+//
+//  Created by Catherine Megregian on 18/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct WhosThatPokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
